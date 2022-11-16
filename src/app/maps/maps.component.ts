e50dd08ebe93cf05f18a475a99bd1ed2a0617c26
W.M.KEVIN <30622305+kephothoX@users.noreply.github.com>
2022-11-16 15:15:59 +0300

    Add files via upload

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -71,7 +71,7 @@ export class MapsComponent implements OnInit, AfterViewInit {
     });
   }
 
-  API_KEY: string = 'AIzaSyCF0eMDF9WX-Hx-OqL-v-C7TgRskObv-Js';
+  API_KEY: string = /*API KEY*/;
 
   MapChoice: string = '';
 
